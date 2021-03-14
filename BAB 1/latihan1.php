554c85b81e0bc119bef7a807e7bd6d1c28ac5862
VitaAyuIndriani <vitaayu136@gmail.com>
2021-03-14 11:29:09 +0700

    revisi latihan1

--- BAB 1/latihan1.php
+++ BAB 1/latihan1.php
@@ -1,23 +1,9 @@
 <?php
 
 class Buku {
-    public $judul,
-        $pengarang,
-        $penerbit,
-        $tahun_terbit,
-        $cetakan;
-
-    public function __construct($judul = "Judul Buku", $pengarang = "Pengarang", $Penerbit = "Penerbit", $tahun_terbit = "Tahun Terbit", $cetakan = "Cetakan") {
-        $this->judul = $jdul;
-        $this->pengarang = $pengarang;
-        $this->penerbit = $penerbit;
-        $this->tahun_terbit = $tahun_terbit;
-        $this->cetakan = $cetakan;
-    }
-}
-class CetakInfoBuku {
-    public function infoBuku( Buku $info ) {
-        $str = "Judul : {$info->jdul} <br> Pengarang : {$info->pengarang} <br> Penerbit : {$info->penerbit} <br> Tahun Terbit : {$info->tahun_terbit} <br> Cetakan : {$info->cetakan}";
-        return $str;
-    }
+    public $judul, 
+           $pengarang, 
+           $penerbit, 
+           $tahun_terbit, 
+           $cetakan;
 }
