12dba0c1637c29037ef2e69cf5a57c6344f1cd8a
VitaAyuIndriani <vitaayu136@gmail.com>
2021-04-13 10:34:36 +0700

    Bab 10

--- bab10/ci/application/views/view_hitunglingkaran.php
+++
@@ -1,5 +0,0 @@
-<?php
-	echo "<br/>";
-	echo '</br> Hasil benchmark (elapsed_time): ' . $this->benchmark->elapsed_time('first', 'second');
-	echo '</br> Hasil benchmark (memory_usage): ' . $this->benchmark->memory_usage('first', 'second');
-?>
